966da34647250ac4f49645ad1d6883afc32505c1
ankitasri2 <ankita.srivastava.a@gmail.com>
2018-09-06 13:34:08 -0500

    your comment for this commit

--- src/com/amb/batch/controller/TransactionsBatchProcessor.java
+++ src/com/amb/batch/controller/TransactionsBatchProcessor.java
@@ -4,7 +4,8 @@ public class TransactionsBatchProcessor {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+		System.out.println("Hiii");
+     System.out.println("hello");
 	}
 
 }
