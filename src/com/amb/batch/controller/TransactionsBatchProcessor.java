d33df669d97822f05bf462927f7f23b8ed71323e
Rajeev <ambasta_rajeev@yahoo.com>
2018-09-06 13:38:56 -0500

    adding message for code indentation

--- src/com/amb/batch/controller/TransactionsBatchProcessor.java
+++ src/com/amb/batch/controller/TransactionsBatchProcessor.java
@@ -5,7 +5,8 @@ public class TransactionsBatchProcessor {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hiii");
-     System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("Hey Ankita!! Please cntrl + A and cntrl + I to indent the code");
 	}
 
 }
